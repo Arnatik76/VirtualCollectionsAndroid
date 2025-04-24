6b9f2ed33a14093828e03d9a09fb322f0a505265
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-04-24 10:52:58 +0500

    Initialize RecyclerView in CatalogFragment and set up GridLayoutManager

--- app/src/main/java/com/example/androidvirtualcollections/CatalogFragment.kt
+++ app/src/main/java/com/example/androidvirtualcollections/CatalogFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 private const val ARG_PARAM1 = "param1"
@@ -21,8 +22,19 @@ class CatalogFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_catalog, container, false)
         recyclerView = view.findViewById(R.id.catalogRecyclerView)
+        setupRecyclerView()
         return view
     }
 
+    private fun setupRecyclerView() {
+        val items = listOf(
+            CatalogItem(1, "Название предмета 1", "Описание предмета 1", R.drawable.ic_catalog),
+            CatalogItem(2, "Название предмета 2", "Описание предмета 2", R.drawable.ic_catalog),
+            CatalogItem(3, "Название предмета 3", "Описание предмета 3", R.drawable.ic_catalog))
+        )
 
+        adapter = CatalogAdapter(items)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+    }
 }

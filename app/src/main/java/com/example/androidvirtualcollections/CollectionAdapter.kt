7ec2651ea9d2ec8b19fafb79ddd014a4003839cb
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-04-24 10:40:06 +0500

    Add view bindings for image, title, and item count in CollectionAdapter ViewHolder

--- app/src/main/java/com/example/androidvirtualcollections/CollectionAdapter.kt
+++ app/src/main/java/com/example/androidvirtualcollections/CollectionAdapter.kt
@@ -3,11 +3,16 @@ package com.example.androidvirtualcollections
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class CollectionAdapter(private val collections: List<Collection>) : RecyclerView.Adapter<CollectionAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.collectionImage)
+        val title: TextView = itemView.findViewById(R.id.collectionTitle)
+        val itemCount: TextView = itemView.findViewById(R.id.collectionItemCount)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {

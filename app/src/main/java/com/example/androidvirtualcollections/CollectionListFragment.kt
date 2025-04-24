22e98d6ade5df07fec6ef3a32f41ec49dc45b230
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-04-24 11:16:27 +0500

    Refactor CollectionListFragment to load collections and set up RecyclerView

--- app/src/main/java/com/example/androidvirtualcollections/CollectionListFragment.kt
+++ app/src/main/java/com/example/androidvirtualcollections/CollectionListFragment.kt
@@ -5,55 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CollectionListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CollectionListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CollectionAdapter
+    private val collections = mutableListOf<Collection>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_collection_list, container, false)
+        recyclerView = view.findViewById(R.id.collectionsRecyclerView)
+        loadCollections()
+        setupRecyclerView()
+        return view
+    }
+
+    private fun loadCollections() {
+        val savedCollections = FileUtils.loadCollections(requireContext())
+        if (savedCollections.isNotEmpty()) {
+            collections.clear()
+            collections.addAll(savedCollections)
+        } else {
+            // Демо-данные
+            collections.clear()
+            collections.addAll(listOf(
+                Collection(1, "Моя коллекция 1", 5, R.drawable.ic_collections),
+                Collection(2, "Моя коллекция 2", 3, R.drawable.ic_collections)
+            ))
+            FileUtils.saveCollections(requireContext(), collections)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_collection_list, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CollectionListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CollectionListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupRecyclerView() {
+        adapter = CollectionAdapter(collections)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 }

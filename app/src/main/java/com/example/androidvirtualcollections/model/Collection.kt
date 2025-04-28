0c43dd3d655a0960756b351adaa4aff9bdf7278d
Arnatik76 <118046506+Arnatik76@users.noreply.github.com>
2025-04-28 12:55:00 +0500

    Implement Parcelable for Collection data class

--- app/src/main/java/com/example/androidvirtualcollections/model/Collection.kt
+++ app/src/main/java/com/example/androidvirtualcollections/model/Collection.kt
@@ -1,7 +1,10 @@
 package com.example.androidvirtualcollections.model
 
+import android.os.Parcelable
 import com.example.androidvirtualcollections.R
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Collection(
     val id: Int,
     val title: String,
@@ -9,4 +12,4 @@ data class Collection(
     val imageUrl: String? = null,
     val imageResId: Int = R.drawable.ic_collections,
     val items: MutableList<MediaItem> = mutableListOf()
-)
+) : Parcelable
